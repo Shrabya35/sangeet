0af42eee94fd496550e58321568c95553c66607a
Shrabya <97744118+Shrabya35@users.noreply.github.com>
2022-10-14 07:51:24 +0545

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+//sticky navbar background
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
