90aa40438dea7b4366ee848b16590220336fbf38
Shrabya <97744118+Shrabya35@users.noreply.github.com>
2022-10-17 18:11:31 +0545

    added new sections

--- style.js
+++ style.js
@@ -5,3 +5,39 @@ function scrollHeader() {
     else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+//new section
+function trend() {
+    document.getElementById("right-body").style.display = "none";
+    document.getElementById("podcast-body").style.display = "none";
+    document.getElementById("trending-body").style.display = "block";
+}
+
+function home() {
+    document.getElementById("right-body").style.display = "block";
+    document.getElementById("trending-body").style.display = "none";
+    document.getElementById("podcast-body").style.display = "none";
+}
+
+function podcast() {
+    document.getElementById("right-body").style.display = "none";
+    document.getElementById("trending-body").style.display = "none";
+    document.getElementById("podcast-body").style.display = "block";
+}
+
+//working buttons
+function like() {
+    document.getElementById('heart').innerHTML = '<i class="ri-heart-line" id="unlike" onclick="unlike();"></i>';
+}
+
+function unlike() {
+    document.getElementById('heart').innerHTML = '<i class="ri-heart-fill" id="like" onclick="like()"></i>';
+}
+
+function mute() {
+    document.getElementById('volume').innerHTML = ' <i class="ri-volume-up-line" id="unmute" onclick = "unmute();"></i>';
+}
+
+function unmute() {
+    document.getElementById('volume').innerHTML = '<i class="ri-volume-mute-fill" id="mute" onclick = "mute();"></i>';
+}
