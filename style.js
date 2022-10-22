a69f12767962b9f1c90cbff7136daf50f88c8a63
Shrabya <97744118+Shrabya35@users.noreply.github.com>
2022-10-22 12:57:57 +0545

    volume bar

--- style.js
+++ style.js
@@ -45,16 +45,22 @@ function unlike() {
 
 function mute() {
     document.getElementById('volume').innerHTML = ' <i class="ri-volume-up-line" id="unmute" onclick = "unmute();"></i>';
+    song.volume = barTwo.value / 100;
 }
 
 function unmute() {
     document.getElementById('volume').innerHTML = '<i class="ri-volume-mute-fill" id="mute" onclick = "mute();"></i>';
+    song.volume = 0;
 }
 
 //modal
 const modal1 = document.querySelector('#login-modal');
+const modal2 = document.querySelector('#signup-modal');
 const openModal1 = document.querySelector('#login');
 const closeModal1 = document.querySelector('#modal-login');
+const closeModal2 = document.querySelector('#modal-signup');
+const logSign = document.querySelector('#log-sign');
+const signLog = document.querySelector('#sign-log');
 
 openModal1.addEventListener('click', () => {
     modal1.showModal();
@@ -62,13 +68,24 @@ openModal1.addEventListener('click', () => {
 closeModal1.addEventListener('click', () => {
     modal1.close();
 })
-
+closeModal2.addEventListener('click', () => {
+    modal2.close();
+})
+logSign.addEventListener('click', () => {
+    modal1.close();
+    modal2.showModal();
+})
+signLog.addEventListener('click', () => {
+    modal2.close();
+    modal1.showModal();
+})
 
 //functional
 let song = new Audio('mp3/1.mp3');
 let songIndex = 0;
 let play = document.getElementById('play');
 let bar = document.getElementById('bar');
+let barTwo = document.getElementById('bar2');
 let songs = [
     { songName: " Never Gonna Give You Up", filePath: "mp3/1.mp3", cover: "img/poster18.jpeg" }
 ]
@@ -93,6 +110,9 @@ song.addEventListener('timeupdate', () => {
 })
 bar.addEventListener('change', () => {
     song.currentTime = bar.value * song.duration / 100;
+})
+barTwo.addEventListener('change', () => {
+    song.volume = barTwo.value / 100;
 })
 
 //nav toggle
